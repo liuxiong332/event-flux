256c8bd04ca29b145825ad19d5c3a05b574dca5b
liuxiong <liuxiong332@163.com>
2018-04-28 11:41:31 +0800

    Fix some bugs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   StoreBase: require('./lib/StoreBase').default,
   StoreRecycleBase: require('./lib/StoreRecycleBase').default,
   Provider: require('./lib/Provider').default,
-  AsyncStorage: require('./AsyncStorage').default,
-  withProvider: require('./withProvider').default,
-  withState: require('./withState').default,
+  AsyncStorage: require('./lib/AsyncStorage').default,
+  withProvider: require('./lib/withProvider').default,
+  withState: require('./lib/withState').default,
 };
