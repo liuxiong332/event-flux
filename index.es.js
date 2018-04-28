636021e818abb5672565d48659b1eab70a81ba2e
liuxiong <liuxiong332@163.com>
2018-04-28 11:01:16 +0800

    Add module file.

---
+++ index.es.js
@@ -0,0 +1,6 @@
+export { default as StoreBase } from './src/StoreBase';
+export { default as StoreRecycleBase } from './StoreRecycleBase';
+export { default as Provider } from './Provider';
+export { default as AsyncStorage } from './AsyncStorage';
+export { default as withProvider } from './withProvider';
+export { default as withState } from './withState';
