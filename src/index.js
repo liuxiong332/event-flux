9c5d34a29d44a4ba59d0bda5ec70962791e043e3
liuxiong <liuxiong332@163.com>
2018-04-28 10:49:37 +0800

    Fix export

--- src/index.js
+++ src/index.js
@@ -4,10 +4,5 @@ import Provider from './Provider';
 import AsyncStorage from './AsyncStorage';
 import withProvider from './withProvider';
 import withState from './withState';
-
-exports.StoreBase = StoreBase;
-exports.StoreRecycleBase = StoreRecycleBase;
-exports.Provider = Provider;
-exports.AsyncStorage = AsyncStorage;
-exports.withProvider = withProvider;
-exports.withState = withState; 
+ 
+export { StoreBase, StoreRecycleBase, Provider, AsyncStorage, withProvider, withState };
