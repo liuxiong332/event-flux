4e7b17e04f1ccd8d19417a5133b537ffd8db17ad
liuxiong <liuxiong332@163.com>
2018-07-10 15:10:58 +0800

    Add SubStoreBase.js

--- packages/electron-event-flux/src/WindowStoreBase.js
+++
@@ -1,8 +0,0 @@
-import StoreBase from 'event-flux/lib/StoreBase';
-
-const IS_WINDOW_STORE = '@@__FLUX_WIN_STORE__@@';
-
-export default class WindowStoreBase extends StoreBase {
-}
-
-WindowStoreBase.prototype[IS_WINDOW_STORE] = true;
