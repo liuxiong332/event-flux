94e9101515aabeb7b3b6297859c3ab01ec87e7f0
liuxiong <liuxiong332@163.com>
2018-11-29 09:52:40 +0800

    Add Provider.js

---
+++ packages/electron-event-flux/src/Provider.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const StoreContext = React.createContext('electron-event-flux');
+const ContextProvider = StoreContext.Provider;
+
+export default class Provider extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.appStore = props.appStore;
+    this.appStore.onChange = this.handleStateChange;
+    this.state = { 
+      _appStore: this.appStore, 
+      stores: this.appStore.stores, 
+      state: this.appStore.state, 
+    };
+  }
+  
+  handleStateChange = (state) => {
+    this.setState({ state });
+  };
+
+  render() {
+    return (
+      <ContextProvider value={this.state}>
+        {React.Children.only(this.props.children)}
+      </ContextProvider>
+    );
+  }
+}
