e9ce565b4145d65daf49aef3bc69940756b0b4ff
liuxiong <liuxiong332@163.com>
2019-01-19 16:52:21 +0800

    Fix the delete logic

--- packages/electron-event-flux/src/utils/__tests__/objectMerge.test.ts
+++ packages/electron-event-flux/src/utils/__tests__/objectMerge.test.ts
@@ -1,6 +1,6 @@
 import objectDifference from '../objectDifference';
 import objectMerge from '../objectMerge';
-import { Map, List, is } from 'immutable';
+import { Map, List, is, fromJS } from 'immutable';
 
 test('base merge', () => {
   let sameObj = { hello: 'a' };
@@ -12,12 +12,42 @@ test('base merge', () => {
 });
 
 test('immutable diff', () => {
-  let val1 = Map({ a: 2, b: 3 });
+  let val1 = Map({ a: 2, b: 3, c: 4 });
   let val2: Map<any, any> = Map({ a: 3, c: 4 });
   val2 = val2.set(10, 10);
 
   let { updated, deleted } = objectDifference(val1, val2);
   let retObj = objectMerge(val1, updated, deleted);
+  expect(retObj.get('b')).toBeUndefined();
   expect(is(retObj, val2)).toBeTruthy();
   expect(retObj.get(10)).toBe(10);
+});
+
+test('immutable diff 2', () => {
+  let oldPortfolio = {
+    portfolioChartInfo: {
+      't_913243980': fromJS({
+        "useIndicatorNames":[],
+        "compareTickers":[],
+        "interval":"m5K",
+        "selectLineType":null,
+        "tickerSettings":{},
+        "timespan":"Day5","chartType":"candles","useShape":"candle"
+      })
+    }
+  };
+  let newPortfolio = {
+    portfolioChartInfo: {
+      't_913243980': fromJS({
+        "useIndicatorNames":[],
+        "compareTickers":[],
+        "selectLineType":null,
+        "tickerSettings":{},
+        "chartType":"candles","useShape":"candle"
+      })
+    }
+  }
+  let { updated, deleted } = objectDifference(oldPortfolio, newPortfolio);
+  let retObj = objectMerge(oldPortfolio, updated, deleted);
+  expect(is(retObj.portfolioChartInfo.t_913243980, newPortfolio.portfolioChartInfo.t_913243980)).toBeTruthy();
 });
--- packages/electron-event-flux/src/utils/objectMerge.ts
+++ packages/electron-event-flux/src/utils/objectMerge.ts
@@ -1,37 +1,50 @@
 import { List, Map } from 'immutable';
 const isObject = require('lodash/isObject');
 const keys = require('lodash/keys');
+const union = require('lodash/union');
 // const { deserialize } = require('json-immutable');
 
 const isShallow = (val) => Array.isArray(val) || !isObject(val) || List.isList(val);
 
 export default function objectMerge(origin, updated, deleted) {
-  if (isShallow(origin) || isShallow(updated)) {
+  /**
+   * origin is shallow
+   * origin is not shallow, updated is shallow and deleted is deleted is empty(NO Update and No Delete)
+   */
+  if (isShallow(origin) || (isShallow(updated) && !isObject(deleted))) {
     return updated;
   }
+
   if (Map.isMap(origin)) {
-    let merged;
-    deleted = isObject(deleted) ? deleted : {};
+    let merged, deleteKeys = [];
+    // deleted = isObject(deleted) ? deleted : {};
     merged = origin.withMutations(map => {
-      keys(deleted).forEach(key => map.delete(JSON.parse(key)));
-      keys(updated).forEach(key => {
+      keys(deleted).forEach(key => {
+        if (deleted[key] === true) {
+          map.delete(JSON.parse(key))
+        } else {
+          deleteKeys.push(key);
+        }
+      });
+      union(keys(updated), deleteKeys).forEach(key => {
         let originKey = JSON.parse(key);
-        map.set(originKey, objectMerge(origin.get(originKey), updated[key], deleted && deleted[key]));
+        map.set(originKey, objectMerge(origin.get(originKey), updated && updated[key], deleted && deleted[key]));
       });
     });
     return merged;
   } else {
-    let merged = {};
+    let merged, deleteKeys;
     if (isObject(deleted)) {
       merged = {};
       keys(origin).forEach(key => {
         if (deleted[key] !== true) merged[key] = origin[key];
       });
+      deleteKeys = keys(deleted).filter(d => deleted[d] !== true)
     } else {
       merged = { ...origin };
     }
-    keys(updated).forEach(key => {
-      merged[key] = objectMerge(origin[key], updated[key], deleted && deleted[key])
+    union(keys(updated), deleteKeys).forEach(key => {
+      merged[key] = objectMerge(origin[key], updated && updated[key], deleted && deleted[key])
     });
     return merged;
   }
